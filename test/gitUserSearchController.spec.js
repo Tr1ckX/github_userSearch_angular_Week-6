cc99066b71044fcac771f92c5903cf7a6a000868
Tr1ckX <attila.mirk@gmail.com>
2014-12-03 21:01:53 +0000

    finihed walkthrough+token

--- test/gitUserSearchController.spec.js
+++ test/gitUserSearchController.spec.js
@@ -15,4 +15,35 @@ describe('GitUserSearchController', function() {
     expect(scope.searchTerm).toBeUndefined();
   });
 
+  describe('when searching for a user', function() {
+
+    var httpBackend;
+    beforeEach(inject(function($httpBackend) {
+      httpBackend = $httpBackend
+      httpBackend
+      .when("GET", "https://api.github.com/search/users?q=bla")
+      .respond({
+        items: items
+      });
+    }));
+
+    var items = [{
+      "login": "tansaku",
+      "avatar_url": "https://avatars.githubusercontent.com/u/30216?v=3",
+      "html_url": "https://github.com/tansaku"
+    }, {
+      "login": "stephenlloyd",
+      "avatar_url": "https://avatars.githubusercontent.com/u/196474?v=3",
+      "html_url": "https://github.com/stephenlloyd"
+    }];
+
+    it('should display search results', function() {
+      scope.searchTerm = 'bla';
+      scope.doSearch();
+      scope.$apply();
+      httpBackend.flush();
+      expect(scope.searchResult.items).toEqual(items);
+    });
+  });
+
 });
